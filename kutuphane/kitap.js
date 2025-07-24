e8f4eec1552af84303cef2e0a40d2df53b8610c0
mt-mobil-lab <serseri_crazy_cocuq@hotmail.com>
2025-07-24 16:21:05 +0300

    Create kitap.js

---
+++ kutuphane/kitap.js
@@ -0,0 +1,45 @@
+  // Sample book data (can be extended or loaded from an API)
+        let books = [
+            {
+                id: 'book1',
+                title: 'kitap1', // kitap adı
+                category: 'Hikaye', // kitap kategorisi
+                pdfUrl: 'kitap1.pdf', // pdf linki
+                coverOverride: 'kitap1.jpg' // kitap fotosu
+            },
+            {
+                id: 'book2',
+                title: 'Küçük Prens',
+                category: 'Hikaye',
+                pdfUrl: 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf', // Updated URL
+                coverOverride: null
+            },
+            {
+                id: 'book3',
+                title: 'Bilim Kurgu Klasikleri',
+                category: 'Bilim Kurgu',
+                pdfUrl: 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf', // Updated URL
+                coverOverride: null
+            },
+            {
+                id: 'book4',
+                title: 'Tarihin Sırları',
+                category: 'Tarih',
+                pdfUrl: 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf', // Updated URL
+                coverOverride: null
+            },
+            {
+                id: 'book5',
+                title: 'Uzay Macerası',
+                category: 'Bilim Kurgu',
+                pdfUrl: 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf', // Updated URL
+                coverOverride: null
+            },
+            {
+                id: 'book6',
+                title: 'Eski Uygarlıklar',
+                category: 'Tarih',
+                pdfUrl: 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf', // Updated URL
+                coverOverride: null
+            }
+        ];
