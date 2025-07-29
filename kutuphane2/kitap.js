8787c05eb16a4abb36c5197494a294a7b31afc5b
mt-mobil-lab <serseri_crazy_cocuq@hotmail.com>
2025-07-29 11:23:37 +0300

    Create kitap.js

---
+++ kutuphane2/kitap.js
@@ -0,0 +1,26 @@
+  const booksData = [
+            {
+                // Bu PDF, FlowPaper tarafından erişilemiyor olabilir.
+                // Eğer bu link çalışmazsa, PDF'i kendi sunucunuzda barındırmanız gerekecektir.
+                pdf: 'https://edebiyat-evi.com/wp-content/uploads/2024/09/Loresima-Gokcen-2-Geride-Kalanlar.pdf', // Pdf için placeholder
+                jpg: 'https://edebiyat-evi.com/wp-content/uploads/2024/09/Loresima-Gokcen-2-Geride-Kalanlar.jpg', // Kapak görseli için placeholder
+                category: 'Aşk Romanı', // Kategori için placeholder
+                author: 'Loresima Gökçen', // Yazar için placeholder
+                title: 'Geride Kalanlar' // Kitap için placeholder
+            },
+            {
+                // Bu PDF, FlowPaper ile çalışması beklenen bir örnektir.
+                pdf: 'https://edebiyat-evi.com/wp-content/uploads/2024/06/beyaz-leke.pdf',
+                jpg: 'https://edebiyat-evi.com/wp-content/uploads/2024/06/Asli-Arslan-BEYAZ-LEKE.jpg',
+                category: 'Aşk Romanı',
+                author: 'Aslı Arslan',
+                title: 'BEYAZ LEKE'
+            },
+            {
+                pdf: 'https://el-kitap.org/wp-content/uploads/2024/01/Pierre-Franckh-Rezonans-kanunu.pdf', // CORS dostu örnek PDF
+                jpg: 'https://el-kitap.org/wp-content/uploads/2024/01/wi_220-10-7-206x300.jpg',
+                category: 'Kişisel Gelişim',
+                author: 'Pierre Franckh',
+                title: 'Rezonans Kanunu'
+            }
+        ];
